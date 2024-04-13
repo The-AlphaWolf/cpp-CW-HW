0da031f50d9ed07aecbb4985cf30c6f1c9244724
Arijit Paul <95159838+The-AlphaWolf@users.noreply.github.com>
2024-04-14 01:05:57 +0530

    Update Binary Search-HW-Week 3.cpp

--- Binary Search-HW-Week 3.cpp
+++ Binary Search-HW-Week 3.cpp
@@ -1,2 +1,39 @@
-#include<iostream>
+
+
+#include <iostream>
+#include<algorithm>
 using namespace std;
+
+int binarySearch(int array[], int x, int s, int e) {
+  while (s <= e) {
+    int mid = (s+e)/2;
+    if (array[mid] == x)
+      return mid;
+    if (array[mid] < x)
+      s = mid + 1;
+    else
+      e = mid - 1;
+  }
+  return -1;
+}
+
+int main() {
+  int n;
+  cout<<"Enter the number of elements : "<<endl;
+  cin>>n;
+  int a,l[n];
+  cout<<"Enter the elements : "<<endl;
+  for(int i=0;i<n;i++){
+      cin>>l[i];
+  }
+  cout<<"Enter the element you want to find : "<<endl;
+  cin>>a;
+  sort(l, l+n);
+  int result = binarySearch(l, a, 0, n - 1);
+  if (result == -1)
+    cout<<"Element not found";
+  else
+    cout<<"Element is found at index : "<<result;
+    
+  return 0;
+}
